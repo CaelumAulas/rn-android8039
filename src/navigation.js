896b857fce12463c42852df6c64e71d05968bcf2
Mario Souto <soutomarios@gmail.com>
2019-03-12 20:42:02 -0300

    update animation

--- src/navigation.js
+++ src/navigation.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, Text, Button } from 'react-native'
 import { createStackNavigator, createAppContainer, createBottomTabNavigator, createSwitchNavigator } from 'react-navigation'
-
+import * as Animatable from 'react-native-animatable'
 // Telas: === 
 
 const LoginScreen = (props) => {
@@ -28,15 +28,32 @@ const ProfileScreen = () => (<View><Text>Profile Screen</Text></View>)
 class SplashScreen extends React.Component {
   componentDidMount() {
     setTimeout(() => {
-      const isUserAuthenticated = true
+      const isUserAuthenticated = false
       this.props.navigation
         .navigate(isUserAuthenticated  ? 'AreaLogado' : 'AreaDeslogado' )
-    }, 300)
+    }, 800)
   }
   render() {
-      return (<View><Text>Instaelum</Text></View>)
+      return (
+      <View style={ { 
+          flex: 1,
+          backgroundColor: 'purple',
+          justifyContent: 'center',
+          alignItems: 'center' }
+        } >
+          <Animatable.Text 
+            style={ { color: 'white', fontSize: 40 } }
+            animation="tada"
+            direction="reverse"
+            iterationCount="infinite"
+            >
+            Instaelum
+          </Animatable.Text>
+      </View>)
   }
 } // Navegação: =======
+
+
 const DeslogadoStack = createStackNavigator({
   Login: { screen: LoginScreen },
 }, { initialRouteName: 'Login' });
